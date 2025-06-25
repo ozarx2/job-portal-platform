c9d305e6784c5e3306fb085f3de8ed28baf5a706
shamseer <hr@ozarx.in>
2025-06-26 00:31:16 +0530

    home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
+        <Route path="/" element={<Home/>}/>
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
